90dd4e9623c0b57942bdf9b0bcd8be891a548a1c
CarmineSgariglia <carminesgariglia2004@gmail.com>
2025-04-10 13:29:48 +0200

    primo test

---
+++ dimensione_tipi/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Dimensione del tipo char e'" << sizeof(char) << '\n';
+    cout << "Dimensione del tipo bool e'" << sizeof(bool) << '\n';
+    cout << "Dimensione del tipo int e'" << sizeof(int) << '\n';
+    cout << "Dimensione del tipo double e'" << sizeof(double) << '\n';
+}
