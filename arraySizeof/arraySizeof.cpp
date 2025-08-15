d16f18f9395196cf5bd5a7eeda86c8b94e000639
CarmineSgariglia <carminesgariglia2004@gmail.com>
2025-08-15 09:41:58 +0200

    eliminazione struttura a cartelle obsoleta

--- arraySizeof/arraySizeof.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int pippo[] {1,2,3,4};
-
-    cout << sizeof(pippo) << endl;
-}
--- dimensione_tipi/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Dimensione del tipo char e'" << sizeof(char) << '\n';
-    cout << "Dimensione del tipo bool e'" << sizeof(bool) << '\n';
-    cout << "Dimensione del tipo int e'" << sizeof(int) << '\n';
-    cout << "Dimensione del tipo double e'" << sizeof(double) << '\n';
-}
